d24d54bdae846c79687895e3d2b67729d09c10b6
William Martin <30499074+wm1995@users.noreply.github.com>
2022-12-16 09:58:59 +0000

    Add tests for new models

--- tests/test_stan.py
+++ tests/test_stan.py
@@ -7,3 +7,23 @@ from tcup.stan import tcup
 def test_tcup(outlier_data, model):
     mcmc = tcup(outlier_data, model=model)
     assert isinstance(mcmc, az.InferenceData)
+
+
+@pytest.mark.parametrize(
+    "prior",
+    [
+        "invgamma",
+        "cauchy",
+        "cauchy_scaled",
+        "cauchy_truncated",
+        "F18",
+        "F18reparam",
+        "nu2",
+        "nu2_principled",
+        "nu2_heuristic",
+        "nu2_scaled",
+    ],
+)
+def test_priors(outlier_data, prior):
+    mcmc = tcup(outlier_data, model="tcup", prior=prior)
+    assert isinstance(mcmc, az.InferenceData)
