c704d04665756f564780625cfdcc4e844c13deb4
William Martin <30499074+wm1995@users.noreply.github.com>
2023-04-07 17:50:41 +0100

    Postprocess samples to calculate true coeffs.

--- tcup/stan.py
+++ tcup/stan.py
@@ -44,6 +44,45 @@ def _prep_data(data):
     return stan_data
 
 
+def _reprocess_samples(scaler, fit):
+    (_, draws, chains) = fit._draws.shape
+    alpha_idx = fit._parameter_indexes("alpha")
+    beta_idx = fit._parameter_indexes("beta")
+    sigma_idx = fit._parameter_indexes("sigma")
+    dim_x = fit.dims[fit.param_names.index("beta")][0]
+
+    alpha_scaled = fit._draws[alpha_idx, :, :].reshape(-1)
+    beta_scaled = fit._draws[beta_idx, :, :].reshape(dim_x, -1)
+    sigma_scaled = fit._draws[sigma_idx, :, :].reshape(-1)
+
+    alpha, beta, sigma = scaler.inv_transform_coeff(
+        alpha_scaled, beta_scaled, sigma_scaled
+    )
+
+    fit.param_names += ("alpha_rescaled",)
+    fit.constrained_param_names += ("alpha_rescaled",)
+    fit.dims += ([],)
+    fit._draws = np.append(fit._draws, alpha.reshape(1, draws, chains), axis=0)
+
+    fit.param_names += ("beta_rescaled",)
+    fit.constrained_param_names += tuple(
+        f"beta_rescaled.{i}" for i in range(dim_x)
+    )
+    fit.dims += ([dim_x],)
+    fit._draws = np.append(
+        fit._draws,
+        beta.reshape(dim_x, draws, chains),
+        axis=0,
+    )
+
+    fit.param_names += ("sigma_rescaled",)
+    fit.constrained_param_names += ("sigma_rescaled",)
+    fit.dims += ([],)
+    fit._draws = np.append(fit._draws, sigma.reshape(1, draws, chains), axis=0)
+
+    return fit
+
+
 def tcup(data, seed=None, model="tcup", **sampler_kwargs):
     x = data.get("x")
     dx = data.get("dx")
@@ -70,4 +109,4 @@ def tcup(data, seed=None, model="tcup", **sampler_kwargs):
     sampler_kwargs.setdefault("num_samples", 1000)
     sampler_kwargs.setdefault("num_chains", 4)
     fit = sampler.sample(**sampler_kwargs)
-    return az.from_pystan(fit)
+    return az.from_pystan(_reprocess_samples(scaler, fit))
