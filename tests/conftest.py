9e9d4f79ff5928629c8cd2fe2e1e60b873e3a928
William Martin <30499074+wm1995@users.noreply.github.com>
2023-04-12 19:22:31 +0100

    Add additional dataset test fixtures

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,6 +5,8 @@ import scipy.stats as sps
 
 def gen_data(rng, alpha, beta, sigma_int, x_true, dx, dy, outlier):
     N = x_true.shape[0]
+    if x_true.ndim > 1:
+        D = x_true.shape[1]
 
     # Set up outlier mask
     outlier_mask = np.ones(N, dtype=bool)
@@ -20,47 +22,67 @@ def gen_data(rng, alpha, beta, sigma_int, x_true, dx, dy, outlier):
 
     # Generate observed values
     x_obs = x_true + sps.norm.rvs(0, dx, size=x_true.shape, random_state=rng)
-    x_cov = np.array([np.identity(x_i.shape[0]) for x_i in x_obs]) * dx**2
+    if x_true.ndim > 1:
+        x_cov = np.array([np.identity(D) for _ in range(N)]) * dx**2
+    else:
+        x_err = np.ones(N) * dx
     y_obs = y_true + sps.norm.rvs(0, dy, size=(N,), random_state=rng)
     y_err = np.ones(N) * dy
 
     # Return data dictionaries
     outlier_data = {
         "x": x_obs,
-        "cov_x": x_cov,
         "y": y_obs,
         "dy": y_err,
     }
+
+    if x_true.ndim > 1:
+        outlier_data["cov_x"] = x_cov
+    else:
+        outlier_data["dx"] = x_err
+
     return outlier_data
 
 
+# def gen_data(rng, alpha, beta, sigma_int, x_true, dx, dy, outlier):
 @pytest.fixture(
     params=[
         {
-            "seed": 24601,
-            "N": 12,
-            "x_range": (0, 10),
-            "obs_err": 0.2,
+            "rng": np.random.default_rng(24601),
+            "alpha": 3,
+            "beta": 2,
+            "sigma_int": 0.1,
+            "x_true": np.linspace(0, 10, 12),
+            "dx": 0.2,
+            "dy": 0.2,
             "outlier": 10,
-            "params": {"alpha": 3, "beta": [2], "sigma_int": 0.1},
-        }
+        },
+        {
+            "rng": np.random.default_rng(24601),
+            "alpha": 3,
+            "beta": [2],
+            "sigma_int": 0.1,
+            "x_true": np.linspace(0, 10, 12)[:, np.newaxis],
+            "dx": 0.2,
+            "dy": 0.2,
+            "outlier": 10,
+        },
+        {
+            "rng": np.random.default_rng(24601),
+            "alpha": -1,
+            "beta": [2, 3],
+            "sigma_int": 0.3,
+            "x_true": np.vstack(
+                [
+                    np.logspace(0, 1, 20),
+                    np.linspace(-1, -4, 20),
+                ]
+            ).T,
+            "dx": 0.2,
+            "dy": 0.2,
+            "outlier": 10,
+        },
     ]
 )
 def data(request):
-    # Set up random number generator
-    rng = np.random.default_rng(request.param["seed"])
-
-    # Create data
-    x_true = np.linspace(*request.param["x_range"], request.param["N"])[
-        :, np.newaxis
-    ]
-    outlier_data = gen_data(
-        rng,
-        x_true=x_true,
-        dx=request.param["obs_err"],
-        dy=request.param["obs_err"],
-        outlier=request.param["outlier"],
-        **request.param["params"],
-    )
-
-    return outlier_data
+    return gen_data(**request.param)
