816d79a6273f2299abb20f18d3361d27279610e8
William Martin <30499074+wm1995@users.noreply.github.com>
2023-04-12 17:28:45 +0100

    Add Stan backend as default tcup backend

--- tcup/__init__.py
+++ tcup/__init__.py
@@ -1,11 +1,12 @@
 from .numpyro import tcup as _tcup_numpyro
+from .stan import tcup as _tcup_stan
 
 
-def tcup(data, seed=None, backend="numpyro", **backend_kwargs):
+def tcup(data, seed=None, backend="stan", **backend_kwargs):
     match backend:
         case "numpyro":
             return _tcup_numpyro(data, seed, **backend_kwargs)
         case "stan":
-            raise NotImplementedError("Stan not yet implemented")
+            return _tcup_stan(data, seed, **backend_kwargs)
         case _:
             raise NotImplementedError(f"Backend {backend} not recognised")
