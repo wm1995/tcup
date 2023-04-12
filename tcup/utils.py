43a5e7625a522cb7ab79157ccd22c3e62f68e9c1
William Martin <30499074+wm1995@users.noreply.github.com>
2023-04-12 17:37:59 +0100

    Remove unneeded utils functions
    I've removed the peak height, outlier fraction and other functions as
    they're not needed in this package.

--- tcup/utils.py
+++ tcup/utils.py
@@ -23,45 +23,6 @@ def suppress_output():
 
 
 @jax.jit
-def peak_height(nu):
-    log_t = 0.5 * jnp.log(2)
-    log_t -= 0.5 * jnp.log(nu)
-    log_t += jspec.gammaln((nu + 1) / 2)
-    log_t -= jspec.gammaln(nu / 2)
-    t = jnp.where(nu == 0.0, 0.0, jnp.exp(log_t))
-    t = jnp.where(jnp.isinf(nu), 1.0, t)
-    return t
-
-
-@jax.jit
-def normality(nu):
-    t = peak_height(nu)
-    c = peak_height(1)  # Cauchy peak height
-    return (t - c) / (1 - c)
-
-
-@jax.jit
-def t_cdf(nu, x):
-    # Using https://encyclopediaofmath.org/wiki/Student_distribution
-    # The above has a source for this in terms of the incomplete Beta function
-    # It's only for x > 0 so I modified it to work for all x
-    # If x < 0, return integral
-    # If x == 0, return 1/2
-    # If x > 0, return 1 - integral
-    integral = 0.5 * tfp_math.betainc(0.5 * nu, 0.5, nu / (nu + x**2))
-    return (jnp.sign(x) + 1) / 2 - jnp.sign(x) * integral
-
-
-@jax.jit
-def outlier_frac(nu, outlier_sigma=3):
-    normal_outlier_frac = 1 - jspec.erf(outlier_sigma / jnp.sqrt(2))
-    omega = tfp_math.betainc(0.5 * nu, 0.5, nu / (nu + outlier_sigma**2))
-    omega = jnp.where(nu == 0, 0.0, omega)
-    omega = jnp.where(jnp.isinf(nu), normal_outlier_frac, omega)
-    return omega
-
-
-@jax.jit
 def sigma_68(nu):
     normal_outlier_frac = 1 - jspec.erf(jnp.sqrt(0.5))
     frac = tfp_math.betaincinv(0.5 * nu, 0.5, normal_outlier_frac)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,34 +2,9 @@ from jax.config import config
 import numpy as np
 import pytest
 import scipy.stats as sps
-from tcup.utils import peak_height, normality, outlier_frac, t_cdf, sigma_68
+from tcup.utils import sigma_68
 
 config.update("jax_enable_x64", True)
-
-
-@pytest.mark.parametrize("nu", [0.1, 0.3, 0.5, 1, 2, 4, 10, 100, 1000])
-@pytest.mark.parametrize("x", [-3, -1, 0, 1, 2, 3, 10])
-def test_t_cdf(nu, x):
-    assert np.isclose(t_cdf(nu, x), sps.t.cdf(x, nu)).all()
-
-
-@pytest.mark.parametrize("nu", [0.1, 0.3, 1, 2, 5, 1000])
-def test_peak_height(nu):
-    t = sps.t.pdf(0, nu) / sps.norm.pdf(0)
-    assert np.isclose(peak_height(nu), t)
-
-
-@pytest.mark.parametrize("nu", [1, 1.5, 2, 3, 10, 1000])
-def test_normality(nu):
-    t = sps.t.pdf(0, nu) - sps.cauchy.pdf(0)
-    t /= sps.norm.pdf(0) - sps.cauchy.pdf(0)
-    assert np.isclose(normality(nu), t)
-
-
-@pytest.mark.parametrize("nu", [0.1, 0.3, 1, 2, 5, 1000])
-def test_outlier_frac(nu):
-    frac = 2 * sps.t.cdf(-3, nu)
-    assert np.isclose(outlier_frac(nu), frac)
 
 
 @pytest.mark.parametrize("nu", [0.1, 0.3, 0.5, 1, 2, 4, 10, 100, 1000])
