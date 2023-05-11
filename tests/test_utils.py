3a68b5ee418360dc3eb0327b79e30365f8073231
William Martin <30499074+wm1995@users.noreply.github.com>
2023-05-11 20:58:53 +0100

    Re-add test for outlier_frac function
    The test was removed in 43a5e76.

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,9 +2,15 @@ from jax.config import config
 import numpy as np
 import pytest
 import scipy.stats as sps
-from tcup.utils import sigma_68
+from tcup.utils import outlier_frac, sigma_68
 
 config.update("jax_enable_x64", True)
+
+
+@pytest.mark.parametrize("nu", [0.1, 0.3, 1, 2, 5, 1000])
+def test_outlier_frac(nu):
+    frac = 2 * sps.t.cdf(-3, nu)
+    assert np.isclose(outlier_frac(nu), frac)
 
 
 @pytest.mark.parametrize("nu", [0.1, 0.3, 0.5, 1, 2, 4, 10, 100, 1000])
