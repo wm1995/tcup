5a5fd20e826068bdd5bab1b9325bc0e29c1cb615
William Martin <30499074+wm1995@users.noreply.github.com>
2023-07-12 16:15:46 +0100

    Update interface to spell out x, y, dy, dx args

--- tcup/__init__.py
+++ tcup/__init__.py
@@ -1,9 +1,22 @@
+from typing import Optional
+
+from numpy.typing import ArrayLike
+
 from .stan import tcup as _tcup_stan
 
 
-def tcup(data, seed=None, backend="stan", **backend_kwargs):
+def tcup(
+    x: ArrayLike,
+    y: ArrayLike,
+    dy: ArrayLike,
+    dx: Optional[ArrayLike] = None,
+    cov_x: Optional[ArrayLike] = None,
+    seed: Optional[int] = None,
+    backend: Optional[str] = "stan",
+    **backend_kwargs,
+):
     match backend:
         case "stan":
-            return _tcup_stan(data, seed, **backend_kwargs)
+            return _tcup_stan(x, y, dy, dx, cov_x, seed=seed, **backend_kwargs)
         case _:
             raise NotImplementedError(f"Backend {backend} not recognised")
