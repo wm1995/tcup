9d1dafe1d32815a0c1a0da246abebaaace4bfc23
William Martin <30499074+wm1995@users.noreply.github.com>
2023-07-17 16:01:24 +0100

    Add new tests for ncup and fixed nu models

--- tests/test_tcup.py
+++ tests/test_tcup.py
@@ -8,3 +8,13 @@ config.update("jax_enable_x64", True)
 def test_tcup(data):
     mcmc = tcup.tcup(**data)
     assert isinstance(mcmc, az.InferenceData)
+
+
+def test_ncup(data):
+    mcmc = tcup.tcup(**data, model="ncup")
+    assert isinstance(mcmc, az.InferenceData)
+
+
+def test_fixed(data):
+    mcmc = tcup.tcup(**data, model="fixed", shape_param=3)
+    assert isinstance(mcmc, az.InferenceData)
